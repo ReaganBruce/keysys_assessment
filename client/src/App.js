15b85f001e4320a2a0cffd439308179f1dd3f7c2
ReaganBruce <reaganbrucebirmingham@gmail.com>
2023-02-03 18:58:57 -0600

    changed comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-//Component and View imports
+//View imports
 import Nav from "./components/Nav.js";
 import Home from "./views/Home.js";
 import Invoices from "./views/Invoices.js";
